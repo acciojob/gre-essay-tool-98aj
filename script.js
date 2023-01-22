fde4d37dd17ce1d209ddcd4686a05c90b0c39bcf
98aj <103376356+98aj@users.noreply.github.com>
2023-01-22 09:24:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your code here
+let inp = document.createElement('input');
+inp.setAttribute('id', 'evaluatedText');
+document.body.append(inp);
+
+let head = document.createElement('h3');
+head.setAttribute('id', 'wordCount');
+document.body.append(head)
+
+let textct = document.getElementById('evaluatedText')
+let text = document.getElementById('wordCount')
+
+text.innerHTML = textct.value.length;
+
+textct.addEventListener('input', cal)
+
+
+
+function cal(){
+    text.innerHTML = textct.value.length;
+}
